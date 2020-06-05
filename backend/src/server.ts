1fc7440ff996305756483060ea9fef81b6826656
Rafael Padovani <r.padovanni@hotmail.com>
2020-06-04 22:42:40 -0300

    Base server.ts for RESTful API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,32 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = ['Rafael', 'Weberson', 'Zé Mané', 'Joberson'];
+
 app.get('/users', (request, response) => {
-    response.send([
-        'Rafael',
-        'Weberson',
-        'Zé Mané',
-        'Joberson'
-    ]);
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter((user) => user.includes(search)) : users;
+
+    return response.send(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    return response.send(users[id]);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email,
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
