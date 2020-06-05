f0e0b17079180fee82bdcc9335a97f0e8ae01940
Rafael Padovani <r.padovanni@hotmail.com>
2020-06-04 22:59:16 -0300

    [BE] Database Settings

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,33 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-const users = ['Rafael', 'Weberson', 'Zé Mané', 'Joberson'];
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter((user) => user.includes(search)) : users;
-
-    return response.send(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-    return response.send(users[id]);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email,
-    };
-
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
